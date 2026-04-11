b92c58b025f315cc84faa7cb40f8ce180c331c58
Ricardo <joaoricardolopes2013@gmail.com>
2026-04-11 19:20:53 -0300

    Bug fix on main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,13 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app = FastAPI(
-    title="Sistema de Compras Online",
-    description="API para gerenciamento de pedidos, produtos, consumidores e vendedores.",
-    version="1.0.0",
-)
-
-
 @app.get("/", tags=["Health"])
 def health_check():
     return {"status": "ok", "message": "API rodando com sucesso!"}
